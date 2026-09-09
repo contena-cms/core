f307e6fb4db11aed7b6f15381f1e9a7c68810196
goxs <lyricsong47@gmail.com>
2026-09-09 15:07:32 +0800

    feat(core): restore custom app flow awareness

---
+++ Content/Flow/Dispatching/Aware/CustomAppAware.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace Contena\Core\Content\Flow\Dispatching\Aware;
+
+use Contena\Core\Framework\Event\IsFlowEventAware;
+
+#[IsFlowEventAware]
+interface CustomAppAware
+{
+    public const string CUSTOM_DATA = 'customAppData';
+
+    /** @return array<string, mixed>|null */
+    public function getCustomAppData(): ?array;
+}
